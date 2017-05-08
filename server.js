441378ed8124c16f15e9084b9b8c414d9a83d1e7
OxanaDoroshkevich <oxana.doroshkevich@student.reutlingen-university.de>
2017-05-08 17:26:24 +0200

    refactored server.js

--- server.js
+++ server.js
@@ -107,9 +107,9 @@ server.listen(port, function () {
 	console.log('Updated : Server listening at port %d', port);
 	});
 
-app.configure(function(){
-	app.use(express.static(__dirname + '/public'));
-	});
+//app.configure(function(){
+//	app.use(express.static(__dirname + '/public'));
+//	});
 
 app.get('*', function (req, res){
 	res.sendfile(__dirname + '/public/index.html');
